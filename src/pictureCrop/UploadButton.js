6dd54a8871cdd9ef4d8c1f1308c13318797c624b
santhosh kumar <santhoshkumar@santhoshs-MacBook-Pro.local>
2021-08-29 11:55:28 +0530

    Santhosh | Add . crop and picture display functionality

---
+++ src/pictureCrop/UploadButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class UploadButton extends React.Component{
+
+
+    render() {
+        return <div>
+            <p>sample</p>
+        </div>;
+      }
+}
+
+
+export default UploadButton;
